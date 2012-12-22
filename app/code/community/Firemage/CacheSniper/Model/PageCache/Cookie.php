d5b5fcd3172667c6732211e76366d92576e4ed29
Scott Weaver <scottmweaver@gmail.com>
2012-12-22 02:10:43 -0800

    Adding in PHPDoc for project files

--- app/code/community/Firemage/CacheSniper/Model/PageCache/Cookie.php
+++ app/code/community/Firemage/CacheSniper/Model/PageCache/Cookie.php
@@ -1,5 +1,24 @@
 <?php
+/**
+ * Firemage CacheSniper
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   Firemage
+ * @package    Firemage_CacheSniper
+ * @author     Scott Weaver <scottmweaver@gmail.com>
+ * @copyright  Copyright (C) 2012 Scott Weaver (http://scottmw.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 
+/**
+ * PageCache Cookie override
+ */
 class Firemage_CacheSniper_Model_PageCache_Cookie extends Enterprise_PageCache_Model_Cookie
 {
     /**
--- app/code/community/Firemage/CacheSniper/controllers/Adminhtml/CachesniperController.php
+++ app/code/community/Firemage/CacheSniper/controllers/Adminhtml/CachesniperController.php
@@ -1,5 +1,24 @@
 <?php
+/**
+ * Firemage CacheSniper
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category   Firemage
+ * @package    Firemage_CacheSniper
+ * @author     Scott Weaver <scottmweaver@gmail.com>
+ * @copyright  Copyright (C) 2012 Scott Weaver (http://scottmw.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
 
+/**
+ * Cache Sniper Main Admin Controller
+ */
 class Firemage_CacheSniper_Adminhtml_CachesniperController extends Mage_Adminhtml_Controller_Action
 {
     public function indexAction()
